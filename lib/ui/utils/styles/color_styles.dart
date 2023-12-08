56254d2143cc425fe8715846f9f001923c1d9b0d
Mohamed Elweza <moahmed9899@gmail.com>
2023-12-08 19:07:34 +0200

    first commit

---
+++ lib/ui/utils/styles/color_styles.dart
@@ -0,0 +1,15 @@
+
+
+import 'dart:ui';
+
+class ColorStyles {
+  ColorStyles._();
+
+  static const Color white = Color(0xffFFFFFF);
+  static const Color red = Color(0xffFF5975);
+  static const Color background = Color(0xff2E1A35);
+  static const Color lightRed = Color(0xffFFF7FA);
+  static const Color lightRed2 = Color(0xffFEDADA);
+  static const Color grey = Color(0xffF1F1EF);
+
+}
